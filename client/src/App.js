68875d19a21ae38a852ca640589247f9fe717eb2
Brycen Medart <brycemedart@gmail.com>
2022-02-27 20:37:54 -0800

    Add imports and routes for pagination

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,27 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import Head from "./pages/Head";
+import Head from "./pages/parts/Head";
 import Home from "./pages/Home";
+import About from './pages/About';
+import Contact from './pages/Contact';
 import Login from "./pages/Login";
-import Foot from "./pages/Foot";
+import Error from "./pages/parts/Error";
+import Foot from "./pages/parts/Foot";
 
 function App() {
   return (
     <BrowserRouter>
+
       <Head />
       <Routes>
         <Route path="/" element={<Home />} exact />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="/login" element={<Login />} />
         <Route path="*" element={<Error />} />
       </Routes>
       <Foot />
+
     </BrowserRouter>
   );
 }
