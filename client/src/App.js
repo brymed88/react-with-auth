8d02bc25f285fff83e3a5a0554944a6720fac609
Brycen Medart <brycemedart@gmail.com>
2022-02-27 20:18:52 -0800

    Add BrowserRouter and routes to main app page.

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import Head from "./pages/Head";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 import Foot from "./pages/Foot";
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
       <Head />
-
+      <Routes>
+        <Route path="/" element={<Home />} exact />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
       <Foot />
-    </div>
+    </BrowserRouter>
   );
 }
 
