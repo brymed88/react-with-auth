33ed46ffb64e013c603e1aa6cd92950ca3c9876b
Brycen Medart <brycemedart@gmail.com>
2022-03-01 12:43:40 -0800

    Removed unused dependencies

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,15 +1,11 @@
-import React from "react";
-import axios from "axios";
 
 import LoginFormComponent from "../components/LoginFormComponent";
 const Login = () => {
-
 
     return (
         <section>
             <LoginFormComponent />
         </section>
-
     );
 }
 
