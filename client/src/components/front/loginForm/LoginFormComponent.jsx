7a4229cf1ad46ddfc4795a04ec4f9a87f3667086
Brycen Medart <brycemedart@gmail.com>
2022-03-24 14:12:20 -0700

    Add functionality to LoginComponent
    - Create error state form when api call fails during login/signup
    -Adjust styles for error messages
    -Add comments to better explain functionality

--- client/src/components/front/loginForm/LoginFormComponent.jsx
+++ client/src/components/front/loginForm/LoginFormComponent.jsx
@@ -9,51 +9,87 @@ import './LoginComponent.min.css';
 
 const LoginFormComponent = () => {
 
+    //Assign useNavigate import to navigate from react-router-dom
     const navigate = useNavigate();
+
+    //De-structure useForm import variables
     const { register, handleSubmit, setError, clearErrors, formState: { errors } } = useForm();
 
+    //Setup state variables for form functionality
     const [formType, setFormType] = useState('login');
     const [loading, setLoading] = useState(false);
+    const [submitError, setSubmitError] = useState(false);
+
     const FormSubmit = (data) => {
+
+        //Reset submit status in case of past failure
+        setSubmitError(false);
+
+        //Set Loading spinner to true while form is processing
+        setLoading(true);
+
         if (formType === 'login') {
-            setLoading(true);
 
             if (data) {
-                //Call helper function to process api call
+
+                //Call util function to process api call
                 UserLogin(data)
                     .then(response => {
 
                         //Successful login, redirect user to dashboard
                         if (response.status === 'success') {
+
+                            //Disable loading spinner as action is now complete
                             setLoading(false);
+
+                            //Redirect to dashboard if login was successful
                             navigate("/dashboard", { replace: true });
+
                         }
                         else {
+
+                            //Set form error for unsuccessful login
+                            setSubmitError(true);
+
+                            //Disable loading spinner as action is now complete
                             setLoading(false);
-                            setError("flogin", { type: "manual", message: "Login Unsuccessful, please try again!" });
+
                         }
                     })
             }
         }
         else {
             if (data.vpassword === data.password) {
+
                 console.log(data);
                 //do fetch call
-                setLoading(true);
+
             }
             else {
+
+                //Set React-Hook-form error for verify password not matching
                 setError("vpassword", { type: "manual", message: "Passwords must match!" });
+
+                //Disable loading spinner as action is now complete
+                setLoading(false);
+
             }
         }
     }
 
     const typeChange = (e) => {
+        //Clear any React-Hook-Forms errors
         clearErrors();
+
+        //Reset submit status in case of past failure
+        setSubmitError(false);
+
         // Set formType state to either login or signup based on target id
         (e.target.id !== 'login' ? setFormType('signup') : setFormType('login'))
     }
 
     return (
+
         <section className='login_form'>
             {loading === true
                 ? <SpinnerComponent type='full' size='100px' />
@@ -66,36 +102,50 @@ const LoginFormComponent = () => {
                     <label className={formType !== 'signup' ? 'label_not_selected' : ''} onClick={typeChange} id="signup" >Signup</label>
                 </div>
 
-                {errors.flogin &&
+                {submitError === true &&
                     <span className="loginError">
                         {/*Check if password mismatch error is set, if not then display generic error*/}
-                        {errors.flogin.message && errors.flogin.message}
+                        Login Unsuccessful, please try again!
                     </span>
                 }
 
                 <div className="inputs">
-                    <input {...register("email", { required: true })} placeholder="Email" />
+
+                    <input {...register("email",
+                        {
+                            required: true,
+                            pattern: {
+                                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i
+                            }
+                        }
+                    )} placeholder="Email" />
+
                     {/* errors will return when field validation fails  */}
-                    {errors.email && <span>This field is required</span>}
+                    {errors.email && <span>This field is required - ex. email@email.com</span>}
+
                 </div>
 
                 <div className="inputs">
+
                     <input {...register("password", { required: true })} placeholder="Password" />
                     {/* errors will return when field validation fails  */}
                     {errors.password && <span>This field is required</span>}
+
                 </div>
 
                 {/* Add the Verify password field conditionally if form type is signup */}
                 {formType === 'signup'
                     &&
                     <div className="inputs">
-                        <input {...register("vpassword", { required: true })} placeholder="Verify Password" />
+
+                        <input {...register("vpassword", { required: true, })} placeholder="Verify Password" />
                         {errors.vpassword &&
                             <span>
                                 {/*Check if password mismatch error is set, if not then display generic error*/}
                                 {errors.vpassword.message ? errors.vpassword.message : 'This field is required'}
                             </span>
                         }
+
                     </div>
                 }
 
@@ -109,6 +159,7 @@ const LoginFormComponent = () => {
                 <div className="login_disclaimer">
                     By creating this account, you agree to our <Link to='/privacy'>Privacy Policy</Link>
                 </div>
+
             </form>
 
         </section>
