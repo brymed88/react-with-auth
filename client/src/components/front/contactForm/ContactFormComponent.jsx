9d4516c90d15c0a5f2eae357cd84e9384543a893
Brycen Medart <brycemedart@gmail.com>
2022-03-24 17:16:56 -0700

    Update ContactComponent validation and styles

--- client/src/components/front/contactForm/ContactFormComponent.jsx
+++ client/src/components/front/contactForm/ContactFormComponent.jsx
@@ -15,7 +15,7 @@ const ContactFormComponent = () => {
     }
 
     return (
-        <section className="contact_wrapper">
+        <section className="contact_form">
             {loading === true
                 && <SpinnerComponent type='full' size='100px' />
             }
@@ -23,18 +23,33 @@ const ContactFormComponent = () => {
             <form onSubmit={handleSubmit(FormSubmit)}>
 
                 <h2>Contact Me</h2>
-                <input {...register("name", { required: true })} placeholder="Name" />
-                {/* errors will return when field validation fails  */}
-                {errors.name && <span>This field is required</span>}
+                <div className="inputs">
+                    <input {...register("name", { required: true })} placeholder="Name" />
+                    {/* errors will return when field validation fails  */}
+                    {errors.name && <span>This field is required</span>}
+                </div>
 
-                <input {...register("email", { required: true })} placeholder="Email" />
-                {/* errors will return when field validation fails  */}
-                {errors.email && <span>This field is required</span>}
+                <div className="inputs">
+                    <input {...register("email",
+                        {
+                            required: true,
+                            pattern: {
+                                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i
+                            }
+                        }
+                    )} placeholder="Email" />
 
-                <textarea {...register("topic", { required: true })} placeholder="Topic of discussion" />
-                {/* errors will return when field validation fails  */}
-                {errors.topic && <span>This field is required</span>}
+                    {/* errors will return when field validation fails  */}
+                    {errors.email && <span>This field is required - ex. email@email.com</span>}
+                </div>
 
+                <div className="inputs">
+                    <textarea {...register("topic", { required: true })} placeholder="Topic of discussion" />
+                    {/* errors will return when field validation fails  */}
+                    {errors.topic && <span>This field is required</span>}
+
+                </div>
+                
                 {/* Change button styles based on form status */}
                 <button
                     className={status === 'Send' ? 'btn' : status === 'Message Sent!' ? 'btn btn_complete' : 'btn btn_failed'}
