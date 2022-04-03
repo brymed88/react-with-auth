b5701c3bb7d38af498f9686147a442e27493fce9
Brycen Medart <brycemedart@gmail.com>
2022-04-02 21:21:38 -0700

    Fix import and add note about nodemailer settings

--- server/src/config/email.js
+++ server/src/config/email.js
@@ -1,6 +1,12 @@
 import nodemailer from 'nodemailer';
-import { log } from "./log.js";
+import { log } from "./logger.js";
+
 export async function sendMail(options) {
+
+    /*
+    * Note: The below function transporter section will change depending on your email host. See documentation 
+    * at https://nodemailer.com/about/. The below example is currently using FastMail. 
+    */
 
     const transporter = nodemailer.createTransport({
         service: "FastMail",
@@ -13,11 +19,11 @@ export async function sendMail(options) {
     const mail = await transporter.sendMail(options);
 
     if (mail.accepted !== '') {
-        log.info("Authorization code sent to user");
+        log.info(`Authorization code sent to ${options.to}`);
         return { status: "sent" };
     }
     else {
-        log.info("Authorization code failed to send");
+        log.info(`Authorization code failed to send to ${options.to}`);
         return { status: "failed" };
     }
 }
