bdaec8a0d4e4b51b0ba3d002cf545662f68472f5
Brycen Medart <brycemedart@gmail.com>
2022-04-04 22:37:28 -0700

    Refactor front-end contact form component

--- client/src/components/front/contactForm/ContactFormComponent.jsx
+++ client/src/components/front/contactForm/ContactFormComponent.jsx
@@ -6,13 +6,50 @@ import SpinnerComponent from '../../common/spinner/SpinnerComponent';
 
 const ContactFormComponent = () => {
 
+    //De-structure useForm import variables
     const { register, handleSubmit, formState: { errors } } = useForm();
+
+    //Setup state variables for form functionality
     const [status, setStatus] = useState('Send');
     const [loading, setLoading] = useState(false);
+    const [submitError, setSubmitError] = useState(false);
 
-    const FormSubmit = (data) => {
+    const FormSubmit = async (data) => {
+
         setLoading(true);
+
+
+
+        //Reset submit status in case of past failure
+        setSubmitError(false);
+
+        if (data) {
+
+            //Call util function to process api call
+            const response = ''//await UserLogin(data);
+
+            //Successful login, redirect user to dashboard
+            if (response.status === 'success') {
+
+                //Disable loading spinner as action is now complete
+                setLoading(false);
+                
+                //Set status of form to sent
+                setStatus('Sent');
+
+            }
+            else {
+
+                //Set form error for unsuccessful login
+                setSubmitError(true);
+
+                //Disable loading spinner as action is now complete
+                setLoading(false);
+
+            }
+        }
     }
+
 
     return (
         <section className="contact_form">
@@ -22,7 +59,15 @@ const ContactFormComponent = () => {
 
             <form onSubmit={handleSubmit(FormSubmit)}>
 
-                <h2>Contact Me</h2>
+                <h2>Contact Form</h2>
+
+                {submitError === true &&
+                    <span className="loginError">
+                        {/*Check if contact form is sent, if not display error*/}
+                        Issue sending form, please try again!
+                    </span>
+                }
+
                 <div className="inputs">
                     <input {...register("name", { required: true })} placeholder="Name" />
                     {/* errors will return when field validation fails  */}
@@ -49,7 +94,7 @@ const ContactFormComponent = () => {
                     {errors.topic && <span>This field is required</span>}
 
                 </div>
-                
+
                 {/* Change button styles based on form status */}
                 <button
                     className={status === 'Send' ? 'btn' : status === 'Message Sent!' ? 'btn btn_complete' : 'btn btn_failed'}
